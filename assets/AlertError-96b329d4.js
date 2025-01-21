a73cc7891da20b4c0681b5a70fbf3f62b18e5dc3
zakariasdiksavas <zakaria.sdik.savas@gmail.com>
2025-01-21 13:18:50 +0100

    remove shadow

---
+++ assets/AlertError-96b329d4.js
@@ -0,0 +1,1 @@
+import{d as s,j as e,l as o}from"./index-5b79c039.js";import{A as t}from"./moment-864131f4.js";function l(){const{t:r,i18n:a}=s();return e.jsx(t,{severity:"error",children:r(o.global.errorMessage)})}export{l as A};
---
+++ assets/use-page-view-5a2741d5.js
@@ -0,0 +1,1 @@
+import{r as e,bc as s}from"./index-5b79c039.js";const a=()=>{e.useEffect(()=>{s.push({event:"page_view"})},[])};export{a as u};
